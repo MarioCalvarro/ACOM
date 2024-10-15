20d04a5607e0dfb73e51c4b686e655ad385c0384
mario <mcalvarro@ucm.es>
2024-10-15 12:26:42 +0200

    Cambios beñat

--- prog4_2.py
+++ prog4_2.py
@@ -1,11 +1,27 @@
 import random
 import math
 
-def gcd(a, b):
-    """ Calcula el máximo común divisor (gcd) de a y b """
-    while b != 0:
-        a, b = b, a % b
-    return a
+def gcd(x, y):
+    """ Calcula el máximo común divisor (gcd) de x e y """
+    x = abs (x)
+    y = abs (y)
+    xespar = x %2 == 0
+    yespar = y %2 == 0
+    if x == 0: # caso base : gcd (0 , y )= y
+        m = y
+    elif y == 0: # caso base : gcd (x ,0)= x
+        m = x
+    elif xespar and yespar :
+        m = 2 * gcd( x //2 , y //2)
+    elif xespar :
+        m = gcd( x //2 , y )
+    elif yespar :
+        m = gcd(x , y //2)
+    elif x > y :
+        m = gcd(y , (x - y )//2)
+    else :
+        m = gcd(x , (y - x )//2)
+    return m
 
 def exp_rapida(base, exp, mod):
     result = 1
@@ -31,18 +47,22 @@ def primos_hasta(n):
 
     return [x for x in range(2, n + 1) if sieve[x]]
 
-def beta(B, n):
-    """ Calcula β como producto de p^ceil(logp(N)) para todos los primos p <= B """
+def expo(a, B, n):
+    """ Calcula a^beta pero haciendo modulo n en cada paso para todos los primos p <= B """
     primes = primos_hasta(B)
-    beta_val = 1
+    beta_val = a
     for p in primes:
-        beta_val *= exp_rapida(p, int(math.ceil(math.log(B, p))), n)
+        potencia = p
+        beta_val = (beta_val ^ p) % n
+        while potencia < n:
+            potencia *= p
+            beta_val = (beta_val ^ p) % n
     return beta_val
 
 def pollard_pm1(n, B):
     """ Implementación del método p-1 de Pollard para factorizar N """
     # Paso 1: Calcular β
-    beta_val = beta(B, n)
+    ##beta_val = beta(B, n)
 
     while True:
         # Paso 2: Elegir a aleatoriamente en [1, N-1]
@@ -56,7 +76,8 @@ def pollard_pm1(n, B):
             return g
 
         # Paso 4: Calcular y = gcd(a^β - 1, N)
-        a_beta = exp_rapida(a, beta_val, n)
+        a_beta = expo(a, B, n)
+        ##a_beta = exp_rapida(a, beta_val, n)
         y = gcd(a_beta - 1, n)
 
         # Si y es un divisor no trivial de N, lo devolvemos
@@ -67,4 +88,8 @@ def pollard_pm1(n, B):
         if y == n:
             continue
 
-pollard_pm1(266941026756980432674273004285227481087654764302423743652775141424617302734658657044184670961906473380670375458124002139355389903179856802994870173312715601752745436095944859694011281380544008461966716092656817602170627934379,1000000)
+#pollard_pm1(266941026756980432674273004285227481087654764302423743652775141424617302734658657044184670961906473380670375458124002139355389903179856802994870173312715601752745436095944859694011281380544008461966716092656817602170627934379,1000000)
+print(pollard_pm1(6762691917284635893988303,100)) # 263381007359
+print(pollard_pm1(119495316416051485815407352901774402921283,1000)) # 45193190000498511563
+print(pollard_pm1(58771624767109592190294695436237320281954733562633238951,10000)) # 455299501714901028162586319
+print(pollard_pm1(266941026756980432674273004285227481087654764302423743652775141424617302734658657044184670961906473380670375458124002139355389903179856802994870173312715601752745436095944859694011281380544008461966716092656817602170627934379,1000000))# 369533287075647816702693047063862986159754020633236293082394185958849017640739268469321177775535208522441080367

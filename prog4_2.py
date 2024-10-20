6fa531fe6f5299a575a36ad9f12eafc802abf768
Diego Ostos <diego.ostos@makingscience.com>
2024-10-20 21:00:21 +0200

    Comentar ejercicio 4

--- prog4_2.py
+++ prog4_2.py
@@ -1,3 +1,14 @@
+'''
+GRUPO: Beñat Pérez de Arenaza Eizaguirre, Mario Calvarro Marines y Diego Ostos Arellano
+Comenzamos en la función principal pollard a la que le llega un número n y un valor B que es el límite superior para determinar 
+si p-1 es B-suave. El objetivo de esta función es encontrar un primo de n utilizando el método p - 1 de Pollard. 
+Comenzamos con la selección de un número aleatorio a entre 2 y n-1, después, calculamos el máximo común divisor(g) entre a y n mediante
+la función gcd(a,n), si g es un divisor no trivial entonces hemos terminado. Si no, hay que calcular y = gcd(a^β -1, n) esto nos devuelve y, 
+si y no es un divisor trivial de n, entonces tenemos nuestra solución y hemos terminado. la función expo(a, B, n) genera todos los primos hasta B
+utilizando el algoritmo de la criba de aristóteles (primos_hasta(B)) y con esto, cada primo p, se eleva a a la potencia de p, repitiendo esto para
+ potencias de p hasta que excedan a n, realizando las operaciones módulo n. 
+'''
+
 import random
 
 def gcd(a, b):
